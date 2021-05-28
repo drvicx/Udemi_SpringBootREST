89e403340fe680629c42bd30cb35cbf90c86d33a
drvicx <nuzhdin.vic@gmail.com>
2021-05-28 11:27:40 +0600

    Create SpringBoot Project (step1)

--- src/main/java/ru/nve/springboot/examples/demoApp1/DemoApp1Application.java
+++ src/main/java/ru/nve/springboot/examples/demoApp1/DemoApp1Application.java
@@ -3,6 +3,7 @@ package ru.nve.springboot.examples.demoApp1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoApp1Application {
 
